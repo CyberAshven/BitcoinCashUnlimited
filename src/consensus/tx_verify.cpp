b1bf4bf9ed259a030c11dff559e1bf30eb73b8ba
Greg-Griffith <probablyaplebeian@protonmail.com>
2020-06-12 22:00:10 -0700

    add proofbase validation rules to CheckTransaction

--- src/consensus/tx_verify.cpp
+++ src/consensus/tx_verify.cpp
@@ -194,8 +194,21 @@ bool CheckTransaction(const CTransactionRef tx, CValidationState &state)
     // Basic checks that don't depend on any context
     if (tx->vin.empty())
         return state.DoS(10, false, REJECT_INVALID, "bad-txns-vin-empty");
-    if (tx->vout.empty())
-        return state.DoS(10, false, REJECT_INVALID, "bad-txns-vout-empty");
+    if (tx->IsProofBase())
+    {
+        if (tx->vout.empty() == false)
+        {
+            return state.DoS(10, false, REJECT_INVALID, "bad-pb-vout-not-empty");
+        }
+    }
+    else
+    {
+        if (tx->vout.empty())
+        {
+            return state.DoS(10, false, REJECT_INVALID, "bad-txns-vout-empty");
+        }
+    }
+
 
     // Sigops moved to ContextualCheckTransaction because the consensus rule goes away after may2020 fork
 
@@ -228,6 +241,10 @@ bool CheckTransaction(const CTransactionRef tx, CValidationState &state)
         // Coinbase tx can't have group outputs because it has no group inputs or mintable outputs
         if (IsAnyTxOutputGrouped(*tx))
             return state.DoS(100, false, REJECT_INVALID, "coinbase-has-group-outputs");
+    }
+    else if (tx->IsProofBase())
+    {
+        // intentionally left blank
     }
     else
     {
