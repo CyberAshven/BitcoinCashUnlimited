RadiantBlockchain/radiant-node

UniValue & RPC Interface: Significant performance improvements
As discussed here: https://gitlab.com/bitcoin-cash-node/bitcoin-cash-node/-/issues/51#note_305079080 in issue #51 , I have observed ways to optimize the UniValue library.
The details of the motivation for the optimizations are discussed in depth in the link above, but the summary is:

- Don't do **O(N)** lookups on each insert into UniValue instances that are VOBJ (dictionaries), by modifying the call site in various places that build particularly large dicts.
- Add move `&&` support for the various `push_*` methods
- Various small nits like making some getters `constexpr` to have the compiler better optimize.
- Use `noexcept` where it's guaranteed `noexcept` for more compiler help.
- Changed some functions that seemed to (un)intentionally break copy elision / RVO for C++ compilers.
- Got rid of some dead code in some places that was building vectors that were never used.

Test Plan
---

**Do the below both against `master` and against this branch, both times by starting from a `ninja clean` build dir**

First, build bitcoin-bench:

    $ ninja src/bench/bitcoin-bench

Some of the performance benefits are huge especially on RPC calls that build large dictionaries.  In particular:

    $ src/bench/bitcoin-bench -filter=RpcMem.\* -evals=20

Other benchmarks that also show improvement:

    $ src/bench/bitcoin-bench -filter=BlockTo.\* -evals=20
    $ src/bench/bitcoin-bench -filter=Json.\* -evals=20  # (this one shows the least improvement)

Additional testing:
If you have testnet synched, build this and start `bitcoind`, hit it with a `bitcoin-cli` request for things like `getrawtransactions true`, `getrawmempool true`, and `getblock` to make sure the results look the same.

I did the above and also I tested this branch against Fulcrum server (synched a full testnet, hit all the RPC commands Fulcrum uses) and nothing broke.

src/core_write.cpp
@@ -196,81 +196,82 @@ void ScriptPubKeyToUniv(const CScript &scriptPubKey, UniValue &out,
     std::vector<CTxDestination> addresses;
     int nRequired;
 
-    out.pushKV("asm", ScriptToAsmStr(scriptPubKey));
+    out.pushKV("asm", ScriptToAsmStr(scriptPubKey), false);
     if (fIncludeHex) {
-        out.pushKV("hex", HexStr(scriptPubKey.begin(), scriptPubKey.end()));
+        out.pushKV("hex", HexStr(scriptPubKey.begin(), scriptPubKey.end()), false);
     }
 
     if (!ExtractDestinations(scriptPubKey, type, addresses, nRequired)) {
-        out.pushKV("type", GetTxnOutputType(type));
+        out.pushKV("type", GetTxnOutputType(type), false);
         return;
     }
 
-    out.pushKV("reqSigs", nRequired);
-    out.pushKV("type", GetTxnOutputType(type));
+    out.pushKV("reqSigs", nRequired, false);
+    out.pushKV("type", GetTxnOutputType(type), false);
 
     UniValue a(UniValue::VARR);
+    a.reserve(addresses.size());
     for (const CTxDestination &addr : addresses) {
         a.push_back(EncodeDestination(addr, GetConfig()));
     }
-    out.pushKV("addresses", a);
+    out.pushKV("addresses", std::move(a), false);
 }
 
 void TxToUniv(const CTransaction &tx, const uint256 &hashBlock, UniValue &entry,
               bool include_hex, int serialize_flags) {
-    entry.pushKV("txid", tx.GetId().GetHex());
-    entry.pushKV("hash", tx.GetHash().GetHex());
-    entry.pushKV("version", tx.nVersion);
-    entry.pushKV("size", (int)::GetSerializeSize(tx, PROTOCOL_VERSION));
-    entry.pushKV("locktime", (int64_t)tx.nLockTime);
+    entry.pushKV("txid", tx.GetId().GetHex(), false);
+    entry.pushKV("hash", tx.GetHash().GetHex(), false);
+    entry.pushKV("version", tx.nVersion, false);
+    entry.pushKV("size", (int)::GetSerializeSize(tx, PROTOCOL_VERSION), false);
+    entry.pushKV("locktime", (int64_t)tx.nLockTime, false);
 
     UniValue vin(UniValue::VARR);
+    vin.reserve(tx.vin.size());
     for (unsigned int i = 0; i < tx.vin.size(); i++) {
         const CTxIn &txin = tx.vin[i];
         UniValue in(UniValue::VOBJ);
         if (tx.IsCoinBase()) {
-            in.pushKV("coinbase",
-                      HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
+            in.pushKV("coinbase", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()), false);
         } else {
-            in.pushKV("txid", txin.prevout.GetTxId().GetHex());
-            in.pushKV("vout", int64_t(txin.prevout.GetN()));
+            in.pushKV("txid", txin.prevout.GetTxId().GetHex(), false);
+            in.pushKV("vout", int64_t(txin.prevout.GetN()), false);
             UniValue o(UniValue::VOBJ);
-            o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true));
-            o.pushKV("hex",
-                     HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
-            in.pushKV("scriptSig", o);
+            o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true), false);
+            o.pushKV("hex", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()), false);
+            in.pushKV("scriptSig", std::move(o), false);
         }
 
-        in.pushKV("sequence", (int64_t)txin.nSequence);
-        vin.push_back(in);
+        in.pushKV("sequence", (int64_t)txin.nSequence, false);
+        vin.push_back(std::move(in));
     }
 
-    entry.pushKV("vin", vin);
+    entry.pushKV("vin", std::move(vin), false);
 
     UniValue vout(UniValue::VARR);
+    vout.reserve(tx.vout.size());
     for (unsigned int i = 0; i < tx.vout.size(); i++) {
         const CTxOut &txout = tx.vout[i];
 
         UniValue out(UniValue::VOBJ);
 
-        out.pushKV("value", ValueFromAmount(txout.nValue));
-        out.pushKV("n", int64_t(i));
+        out.pushKV("value", ValueFromAmount(txout.nValue), false);
+        out.pushKV("n", int64_t(i), false);
 
         UniValue o(UniValue::VOBJ);
         ScriptPubKeyToUniv(txout.scriptPubKey, o, true);
-        out.pushKV("scriptPubKey", o);
-        vout.push_back(out);
+        out.pushKV("scriptPubKey", std::move(o), false);
+        vout.push_back(std::move(out));
     }
 
-    entry.pushKV("vout", vout);
+    entry.pushKV("vout", std::move(vout), false);
 
     if (!hashBlock.IsNull()) {
-        entry.pushKV("blockhash", hashBlock.GetHex());
+        entry.pushKV("blockhash", hashBlock.GetHex(), false);
     }
 
     if (include_hex) {
         // The hex-encoded transaction. Used the name "hex" to be consistent
         // with the verbose output of "getrawtransaction".
-        entry.pushKV("hex", EncodeHexTx(tx, serialize_flags));
+        entry.pushKV("hex", EncodeHexTx(tx, serialize_flags), false);
     }
 }

src/rpc/mining.cpp
@@ -605,6 +605,7 @@ static UniValue getblocktemplate(const Config &config,
     aCaps.push_back("proposal");
 
     UniValue transactions(UniValue::VARR);
+    transactions.reserve(pblock->vtx.size());
     int index_in_template = 0;
     for (const auto &it : pblock->vtx) {
         const CTransaction &tx = *it;
@@ -616,16 +617,14 @@ static UniValue getblocktemplate(const Config &config,
         }
 
         UniValue entry(UniValue::VOBJ);
-        entry.pushKV("data", EncodeHexTx(tx));
-        entry.pushKV("txid", txId.GetHex());
-        entry.pushKV("hash", tx.GetHash().GetHex());
-        entry.pushKV("fee",
-                     pblocktemplate->entries[index_in_template].fees / SATOSHI);
-        int64_t nTxSigOps =
-            pblocktemplate->entries[index_in_template].sigOpCount;
-        entry.pushKV("sigops", nTxSigOps);
+        entry.pushKV("data", EncodeHexTx(tx), false);
+        entry.pushKV("txid", txId.GetHex(), false);
+        entry.pushKV("hash", tx.GetHash().GetHex(), false);
+        entry.pushKV("fee", pblocktemplate->entries[index_in_template].fees / SATOSHI, false);
+        int64_t nTxSigOps = pblocktemplate->entries[index_in_template].sigOpCount;
+        entry.pushKV("sigops", nTxSigOps, false);
 
-        transactions.push_back(entry);
+        transactions.push_back(std::move(entry));
         index_in_template++;
     }
 
@@ -640,27 +639,27 @@ static UniValue getblocktemplate(const Config &config,
     aMutable.push_back("prevblock");
 
     UniValue result(UniValue::VOBJ);
-    result.pushKV("capabilities", aCaps);
+    result.pushKV("capabilities", std::move(aCaps), false);
 
-    result.pushKV("version", pblock->nVersion);
+    result.pushKV("version", pblock->nVersion, false);
 
-    result.pushKV("previousblockhash", pblock->hashPrevBlock.GetHex());
-    result.pushKV("transactions", transactions);
-    result.pushKV("coinbaseaux", aux);
-    result.pushKV("coinbasevalue",
-                  int64_t(pblock->vtx[0]->vout[0].nValue / SATOSHI));
-    result.pushKV("longpollid", chainActive.Tip()->GetBlockHash().GetHex() +
-                                    i64tostr(nTransactionsUpdatedLast));
-    result.pushKV("target", hashTarget.GetHex());
-    result.pushKV("mintime", int64_t(pindexPrev->GetMedianTimePast()) + 1);
-    result.pushKV("mutable", aMutable);
-    result.pushKV("noncerange", "00000000ffffffff");
+    result.pushKV("previousblockhash", pblock->hashPrevBlock.GetHex(), false);
+    result.pushKV("transactions", std::move(transactions), false);
+    result.pushKV("coinbaseaux", std::move(aux), false);
+    result.pushKV("coinbasevalue", int64_t(pblock->vtx[0]->vout[0].nValue / SATOSHI), false);
+    result.pushKV("longpollid",
+                  chainActive.Tip()->GetBlockHash().GetHex() + i64tostr(nTransactionsUpdatedLast),
+                  false);
+    result.pushKV("target", hashTarget.GetHex(), false);
+    result.pushKV("mintime", int64_t(pindexPrev->GetMedianTimePast()) + 1, false);
+    result.pushKV("mutable", std::move(aMutable), false);
+    result.pushKV("noncerange", "00000000ffffffff", false);
     // FIXME: Allow for mining block greater than 1M.
-    result.pushKV("sigoplimit", GetMaxBlockSigOpsCount(DEFAULT_MAX_BLOCK_SIZE));
-    result.pushKV("sizelimit", DEFAULT_MAX_BLOCK_SIZE);
-    result.pushKV("curtime", pblock->GetBlockTime());
-    result.pushKV("bits", strprintf("%08x", pblock->nBits));
-    result.pushKV("height", int64_t(pindexPrev->nHeight) + 1);
+    result.pushKV("sigoplimit", GetMaxBlockSigOpsCount(DEFAULT_MAX_BLOCK_SIZE), false);
+    result.pushKV("sizelimit", DEFAULT_MAX_BLOCK_SIZE, false);
+    result.pushKV("curtime", pblock->GetBlockTime(), false);
+    result.pushKV("bits", strprintf("%08x", pblock->nBits), false);
+    result.pushKV("height", int64_t(pindexPrev->nHeight) + 1, false);
 
     return result;
 }

CyberAshven/BitcoinCashUnlimited

This is a port of  bitcoin-cash-node/bitcoin-cash-node!12

src/core_write.cpp
@@ -164,54 +164,59 @@ void ScriptPubKeyToUniv(const CScript &scriptPubKey, UniValue &out, bool fInclud
     vector<CTxDestination> addresses;
     int nRequired;
 
-    out.pushKV("asm", ScriptToAsmStr(scriptPubKey));
+    out.pushKV("asm", ScriptToAsmStr(scriptPubKey), false);
     if (fIncludeHex)
-        out.pushKV("hex", HexStr(scriptPubKey.begin(), scriptPubKey.end()));
+        out.pushKV("hex", HexStr(scriptPubKey.begin(), scriptPubKey.end()), false);
 
     if (!ExtractDestinations(scriptPubKey, type, addresses, nRequired))
     {
-        out.pushKV("type", GetTxnOutputType(type));
+        out.pushKV("type", GetTxnOutputType(type), false);
         return;
     }
 
-    out.pushKV("reqSigs", nRequired);
-    out.pushKV("type", GetTxnOutputType(type));
+    out.pushKV("reqSigs", nRequired, false);
+    out.pushKV("type", GetTxnOutputType(type), false);
 
     UniValue a(UniValue::VARR);
+    a.reserve(addresses.size());
+
     for (const CTxDestination &addr : addresses)
     {
         a.push_back(EncodeDestination(addr));
     }
-    out.pushKV("addresses", a);
+    out.pushKV("addresses", std::move(a), false);
 }
 
 void TxToUniv(const CTransaction &tx, const uint256 &hashBlock, UniValue &entry)
 {
-    entry.pushKV("txid", tx.GetHash().GetHex());
-    entry.pushKV("version", tx.nVersion);
-    entry.pushKV("locktime", (int64_t)tx.nLockTime);
+    entry.pushKV("txid", tx.GetHash().GetHex(), false);
+    entry.pushKV("version", tx.nVersion, false);
+    entry.pushKV("locktime", (int64_t)tx.nLockTime, false);
+    // TODO add size field
 
     UniValue vin(UniValue::VARR);
+    vin.reserve(tx.vin.size());
     for (const CTxIn &txin : tx.vin)
     {
         UniValue in(UniValue::VOBJ);
         if (tx.IsCoinBase())
-            in.pushKV("coinbase", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
+            in.pushKV("coinbase", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()), false);
         else
         {
-            in.pushKV("txid", txin.prevout.hash.GetHex());
-            in.pushKV("vout", (int64_t)txin.prevout.n);
+            in.pushKV("txid", txin.prevout.hash.GetHex(), false);
+            in.pushKV("vout", (int64_t)txin.prevout.n, false);
             UniValue o(UniValue::VOBJ);
-            o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true));
-            o.pushKV("hex", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()));
-            in.pushKV("scriptSig", o);
+            o.pushKV("asm", ScriptToAsmStr(txin.scriptSig, true), false);
+            o.pushKV("hex", HexStr(txin.scriptSig.begin(), txin.scriptSig.end()), false);
+            in.pushKV("scriptSig", o, false);
         }
-        in.pushKV("sequence", (int64_t)txin.nSequence);
-        vin.push_back(in);
+        in.pushKV("sequence", (int64_t)txin.nSequence, false);
+        vin.push_back(std::move(in));
     }
-    entry.pushKV("vin", vin);
+    entry.pushKV("vin", std::move(vin), false);
 
     UniValue vout(UniValue::VARR);
+    vout.reserve(tx.vout.size());
     for (unsigned int i = 0; i < tx.vout.size(); i++)
     {
         const CTxOut &txout = tx.vout[i];
@@ -219,19 +224,19 @@ void TxToUniv(const CTransaction &tx, const uint256 &hashBlock, UniValue &entry)
         UniValue out(UniValue::VOBJ);
 
         UniValue outValue(UniValue::VNUM, FormatMoney(txout.nValue));
-        out.pushKV("value", outValue);
-        out.pushKV("n", (int64_t)i);
+        out.pushKV("value", outValue, false);
+        out.pushKV("n", (int64_t)i, false);
 
         UniValue o(UniValue::VOBJ);
         ScriptPubKeyToUniv(txout.scriptPubKey, o, true);
-        out.pushKV("scriptPubKey", o);
-        vout.push_back(out);
+        out.pushKV("scriptPubKey", std::move(o), false);
+        vout.push_back(std::move(out));
     }
-    entry.pushKV("vout", vout);
+    entry.pushKV("vout", std::move(vout), false);
 
     if (!hashBlock.IsNull())
-        entry.pushKV("blockhash", hashBlock.GetHex());
+        entry.pushKV("blockhash", hashBlock.GetHex(), false);
 
     // the hex-encoded transaction. used the name "hex" to be consistent with the verbose output of "getrawtransaction".
-    entry.pushKV("hex", EncodeHexTx(tx));
+    entry.pushKV("hex", EncodeHexTx(tx), false);
 }

src/rpc/mining.cpp
@@ -437,6 +437,7 @@ static UniValue MkFullMiningCandidateJson(std::set<std::string> setClientRules,
     aCaps.push_back("proposal");
 
     UniValue transactions(UniValue::VARR);
+    transactions.reserve(pblock->vtx.size());
     map<uint256, int64_t> setTxIndex;
     int i = 0;
     int sigcheckTotal = 0;
@@ -451,9 +452,9 @@ static UniValue MkFullMiningCandidateJson(std::set<std::string> setClientRules,
 
         UniValue entry(UniValue::VOBJ);
 
-        entry.pushKV("data", EncodeHexTx(tx));
+        entry.pushKV("data", EncodeHexTx(tx), false);
 
-        entry.pushKV("hash", txHash.GetHex());
+        entry.pushKV("hash", txHash.GetHex(), false);
 
         UniValue deps(UniValue::VARR);
         for (const CTxIn &in : tx.vin)
@@ -466,16 +467,16 @@ static UniValue MkFullMiningCandidateJson(std::set<std::string> setClientRules,
         int index_in_template = i - 1;
         entry.pushKV("fee", pblocktemplate->vTxFees[index_in_template]);
         if (!may2020Enabled)
-            entry.pushKV("sigops", pblocktemplate->vTxSigOps[index_in_template]);
+            entry.pushKV("sigops", pblocktemplate->vTxSigOps[index_in_template], false);
         else
         {
             // sigops is deprecated and not part of this block's consensus so report 0
-            entry.pushKV("sigops", 0);
-            entry.pushKV("sigchecks", pblocktemplate->vTxSigOps[index_in_template]);
+            entry.pushKV("sigops", 0, false);
+            entry.pushKV("sigchecks", pblocktemplate->vTxSigOps[index_in_template], false);
             sigcheckTotal += pblocktemplate->vTxSigOps[index_in_template];
         }
 
-        transactions.push_back(entry);
+        transactions.push_back(std::move(entry));
     }
 
     UniValue aRules(UniValue::VARR);
@@ -498,11 +499,11 @@ static UniValue MkFullMiningCandidateJson(std::set<std::string> setClientRules,
     aMutable.push_back("prevblock");
 
     UniValue result(UniValue::VOBJ);
-    result.pushKV("capabilities", aCaps);
-    result.pushKV("version", pblock->nVersion);
-    result.pushKV("rules", aRules);
-    result.pushKV("vbavailable", vbavailable);
-    result.pushKV("vbrequired", int(0));
+    result.pushKV("capabilities", aCaps, false);
+    result.pushKV("version", pblock->nVersion, false);
+    result.pushKV("rules", aRules, false);
+    result.pushKV("vbavailable", vbavailable, false);
+    result.pushKV("vbrequired", int(0), false);
 
     if (nMaxVersionPreVB >= 2)
     {
@@ -516,29 +517,29 @@ static UniValue MkFullMiningCandidateJson(std::set<std::string> setClientRules,
         aMutable.push_back("version/force");
     }
 
-    result.pushKV("previousblockhash", pblock->hashPrevBlock.GetHex());
-    result.pushKV("transactions", transactions);
-    result.pushKV("coinbaseaux", aux);
-    result.pushKV("coinbasevalue", (int64_t)pblock->vtx[0]->vout[0].nValue);
-    result.pushKV("longpollid", chainActive.Tip()->GetBlockHash().GetHex() + i64tostr(nTransactionsUpdatedLast));
-    result.pushKV("target", hashTarget.GetHex());
-    result.pushKV("mintime", (int64_t)pindexPrev->GetMedianTimePast() + 1);
-    result.pushKV("mutable", aMutable);
-    result.pushKV("noncerange", "00000000ffffffff");
+    result.pushKV("previousblockhash", pblock->hashPrevBlock.GetHex(), false);
+    result.pushKV("transactions", transactions, false);
+    result.pushKV("coinbaseaux", aux, false);
+    result.pushKV("coinbasevalue", (int64_t)pblock->vtx[0]->vout[0].nValue, false);
+    result.pushKV("longpollid", chainActive.Tip()->GetBlockHash().GetHex() + i64tostr(nTransactionsUpdatedLast), false);
+    result.pushKV("target", hashTarget.GetHex(), false);
+    result.pushKV("mintime", (int64_t)pindexPrev->GetMedianTimePast() + 1, false);
+    result.pushKV("mutable", aMutable, false);
+    result.pushKV("noncerange", "00000000ffffffff", false);
 
     // Deprecated after may 2020 but leave it in in case miners are using it in their code.
-    result.pushKV("sigoplimit", (int64_t)MAX_BLOCK_SIGOPS_PER_MB);
+    result.pushKV("sigoplimit", (int64_t)MAX_BLOCK_SIGOPS_PER_MB, false);
     if (may2020Enabled)
     {
-        result.pushKV("sigchecklimit", maxSigChecks.Value());
-        result.pushKV("sigchecktotal", sigcheckTotal);
+        result.pushKV("sigchecklimit", maxSigChecks.Value(), false);
+        result.pushKV("sigchecktotal", sigcheckTotal, false);
     }
 
-    result.pushKV("sizelimit", (int64_t)maxGeneratedBlock);
-    result.pushKV("curtime", pblock->GetBlockTime());
-    result.pushKV("bits", strprintf("%08x", pblock->nBits));
+    result.pushKV("sizelimit", (int64_t)maxGeneratedBlock, false);
+    result.pushKV("curtime", pblock->GetBlockTime(), false);
+    result.pushKV("bits", strprintf("%08x", pblock->nBits), false);
     // BU get the height directly from the block because pindexPrev could change if another block has come in.
-    result.pushKV("height", (int64_t)(pblock->GetHeight()));
+    result.pushKV("height", (int64_t)(pblock->GetHeight()), false);
 
     return result;
 }