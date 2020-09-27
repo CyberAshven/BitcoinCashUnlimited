6fc0003831bb6f4f96bf1ec57b79e4ae671730c5
Griffith <probablyaplebeian@protonmail.com>
2020-09-27 01:53:05 -0700

    remove AD from subversion check tests

--- src/globals.cpp
+++ src/globals.cpp
@@ -281,7 +281,7 @@ CTweakRef<uint64_t> ebTweak("net.excessiveBlock",
     "Excessive block size in bytes",
     &excessiveBlockSize,
     &ExcessiveBlockValidator);
-    
+
 CTweak<bool> ignoreNetTimeouts("net.ignoreTimeouts", "ignore inactivity timeouts, used during debugging", false);
 CTweakRef<bool> displayArchInSubver("net.displayArchInSubver",
     "Show box architecture, 32/64bit, in node user agent string (subver)",
