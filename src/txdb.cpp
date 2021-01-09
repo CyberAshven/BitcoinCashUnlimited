7740dd0eba4652eb82516f54c698a7067bfc09e8
Griffith <probablyaplebeian@protonmail.com>
2021-01-09 12:37:05 -0800

    rework CCoinsViewDB BatchWrite to use new iteration method for CCoinsMap

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -191,7 +191,9 @@ bool CCoinsViewDB::BatchWrite(CCoinsMap &mapCoins,
     size_t nBatchWrites = 0;
     size_t batch_size = nMaxDBBatchSize;
 
-    for (CCoinsMap::iterator it = mapCoins.begin(); it != mapCoins.end();)
+    CCoinsMap::iterator it = mapCoins.begin();
+    uint8_t i = 0;
+    while(it != mapCoins.end())
     {
         if (it->second.flags & CCoinsCacheEntry::DIRTY)
         {
@@ -200,15 +202,13 @@ bool CCoinsViewDB::BatchWrite(CCoinsMap &mapCoins,
             if (it->second.coin.IsSpent())
             {
                 batch.Erase(entry);
-
                 // Update the usage of the child cache before deleting the entry in the child cache
                 nChildCachedCoinsUsage -= nUsage;
-                it = mapCoins.erase(it);
+                it = mapCoins.erase(i, it);
             }
             else
             {
                 batch.Write(entry, it->second.coin);
-
                 // Only delete valid coins from the cache when we're nearly syncd.  During IBD, and also
                 // if BlockOnly mode is turned on, these coins will be used, whereas, once the chain is
                 // syncd we only need the coins that have come from accepting txns into the memory pool.
@@ -217,16 +217,15 @@ bool CCoinsViewDB::BatchWrite(CCoinsMap &mapCoins,
                 {
                     // Update the usage of the child cache before deleting the entry in the child cache
                     nChildCachedCoinsUsage -= nUsage;
-                    it = mapCoins.erase(it);
+                    it = mapCoins.erase(i, it);
                 }
                 else
                 {
                     it->second.flags = 0;
-                    it++;
+                    mapCoins.next(i, it);
                 }
             }
             changed++;
-
             // In order to prevent the spikes in memory usage that used to happen when we prepared large as
             // was possible, we instead break up the batches such that the performance gains for writing to
             // leveldb are still realized but the memory spikes are not seen.
@@ -238,11 +237,15 @@ bool CCoinsViewDB::BatchWrite(CCoinsMap &mapCoins,
             }
         }
         else
-            it++;
+        {
+            mapCoins.next(i, it);
+        }
         count++;
     }
     if (!hashBlock.IsNull())
-        _WriteBestBlock(hashBlock);
+    {
+        WriteBestBlock(hashBlock);
+    }
 
     bool ret = db.WriteBatch(batch);
     LOG(COINDB, "Committing %u changed transactions (out of %u) to coin database with %u batch writes...\n",
