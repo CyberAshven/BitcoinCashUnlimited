87d71307dac42eb2a7c62f4c48208628aebd06f9
Griffith <probablyaplebeian@protonmail.com>
2021-02-06 19:29:30 -0800

    uncomment check for too many blocks in flight

--- src/blockrelay/blockrelay_common.cpp
+++ src/blockrelay/blockrelay_common.cpp
@@ -239,9 +239,8 @@ void ThinTypeRelay::BlockWasReceived(CNode *pfrom, const uint256 &hash)
 bool ThinTypeRelay::AddBlockInFlight(CNode *pfrom, const uint256 &hash, const std::string thinType)
 {
     LOCK(cs_inflight);
-    //TODO: Figure out why BobCompactBlocks do not work with this
-    //if (AreTooManyBlocksInFlight())
-    //    return false;
+    if (AreTooManyBlocksInFlight())
+        return false;
 
     // this insert returns a pair <iterator,bool> where the bool denotes whether the insertion took place
     auto key = mapThinTypeBlocksInFlight.find(pfrom->GetId());
