4888635eaab68192ead32d28238fbcc02da0b9ea
George Bissias <bissias@gmail.com>
2021-04-15 13:20:41 -0400

    add serialization test for tailstorm merkle block

--- src/test/tailstorm_serialization_objs.cpp
+++ src/test/tailstorm_serialization_objs.cpp
@@ -1,6 +1,7 @@
 #include "bloom.h"
 #include "consensus/merkle.h"
 #include "streams.h"
+#include "tailstorm/block/merkletailblock.h"
 #include "tailstorm/subblock/merklesubblock.h"
 #include "tailstorm/tailstorm.h"
 #include "test/test_bitcoin.h"
@@ -15,7 +16,6 @@ BOOST_FIXTURE_TEST_SUITE(tailstorm_serialization_objs, BasicTestingSetup)
 
 BOOST_AUTO_TEST_CASE(generate_merkle_block_hex)
 {
-    
     // Set up objects
     CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
     CMutableTransaction mtx;
@@ -53,7 +53,6 @@ BOOST_AUTO_TEST_CASE(generate_merkle_block_hex)
 
 BOOST_AUTO_TEST_CASE(generate_merkle_subblock_hex)
 {
-    
     // Set up objects
     CBloomFilter filter(10, 0.000001, 0, BLOOM_UPDATE_ALL);
     CMutableTransaction mtx;
@@ -85,6 +84,46 @@ BOOST_AUTO_TEST_CASE(generate_merkle_subblock_hex)
     // Write to file
     std::ofstream outfile;
     outfile.open ("/tmp/generate_merkle_subblock_hex.dat");
+    outfile << hexStr;
+    outfile.close();
+}
+
+BOOST_AUTO_TEST_CASE(generate_merkle_tailblock_hex)
+{
+    // Set up objects
+    CMutableTransaction mtx;
+    mtx.vin.resize(1);
+    mtx.vin[0].prevout.n = 11;
+    CTransactionRef tx = std::make_shared<const CTransaction>(mtx);
+    std::set<uint256> txids;
+    txids.insert(tx->GetHash());
+    CSubBlockRef subref = std::make_shared<CSubBlock>();
+    subref->vtx.push_back(tx);
+    subref->hashMerkleRoot = BlockMerkleRoot(*subref);
+    CTailstormBlock block;
+    block.vdag.push_back(subref);
+    block.UpdateTxLists();
+    CMerkleTailBlock mtb(block, txids);
+
+    // Serialize
+    CDataStream ssMerBlk(SER_NETWORK, PROTOCOL_VERSION);
+    ssMerBlk << mtb;
+    std::string hexStr = HexStr(ssMerBlk.begin(), ssMerBlk.end());
+
+    // Deserialize
+    CMerkleTailBlock mtb2;
+    std::vector<unsigned char> mtbData(ParseHex(hexStr));
+    CDataStream ssData(mtbData, SER_NETWORK, PROTOCOL_VERSION);
+    ssData >> mtb2;
+    BOOST_CHECK(mtb2.header.GetHash() == mtb.header.GetHash());
+    std::vector<uint256> vMatch;
+    std::vector<unsigned int> vnIndex;
+    mtb2.subblocks[0].txn.ExtractMatches(vMatch, vnIndex);
+    BOOST_CHECK(vMatch[0] == tx->GetHash());
+
+    // Write to file
+    std::ofstream outfile;
+    outfile.open ("/tmp/generate_merkle_tailblock_hex.dat");
     outfile << hexStr;
     outfile.close();
 }
