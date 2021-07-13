45d6df7ba433f467d77693fdc4ec0e58e5b63421
George Bissias <bissias@gmail.com>
2021-07-12 20:06:00 -0400

    rebase is now compiling but bobtail qa tests are failing

--- src/validation/validation.cpp
+++ src/validation/validation.cpp
@@ -56,40 +56,6 @@ std::unordered_set<uint256, Hasher> setBlocksAlreadyChecked GUARDED_BY(cs_Blocks
 // We don't let this set grow unbounded just in case we forget to erase values later.
 const unsigned int MAX_SETBLOCKSALREADYCHECKED_SIZE = 5000;
 
-struct CBlockIndexWorkComparator
-{
-    bool operator()(CBlockIndex *pa, CBlockIndex *pb) const
-    {
-        // First sort by most total work, ...
-        if (pa->nChainWork > pb->nChainWork)
-            return false;
-        if (pa->nChainWork < pb->nChainWork)
-            return true;
-
-        // ... then by block arrival sequence.
-        //
-        // Although in general the sequence id orders block by time, in the case of a block race, the
-        // sequence id can be swapped such that the winning block will have the lower sequence_id. This
-        // swapping of id's only is important when/if the node is shutdown and restarts where there were
-        // two or more blocks present at the same height. Then upon restarting the node will stay on the
-        // same block before shutdown regardless of the original time of arrival.
-        if (pa->nSequenceId < pb->nSequenceId)
-            return false;
-        if (pa->nSequenceId > pb->nSequenceId)
-            return true;
-
-        // Use pointer address as tie breaker (should only happen with blocks
-        // loaded from disk, as those all have id 0).
-        if (pa < pb)
-            return false;
-        if (pa > pb)
-            return true;
-
-        // Identical blocks.
-        return false;
-    }
-};
-
 // bip135 begin
 // keep track of count over last 100
 struct UnknownForkData
@@ -1887,7 +1853,7 @@ bool AcceptBlock(const CBlock &block,
         }
         if (dbp == nullptr)
         {
-            if (!WriteBlockToDisk(block, blockPos, chainparams.MessageStart(), &nHeight))
+            if (!WriteBlockToDisk(block, blockPos, chainparams.MessageStart()))
             {
                 AbortNode(state, "Failed to write block");
             }
@@ -3192,7 +3158,8 @@ bool DisconnectTip(CValidationState &state, const Consensus::Params &consensusPa
     CBlockIndex *pindexDelete = chainActive.Tip();
     assert(pindexDelete);
     // Read block from disk.
-    CBlockRef pblock = ReadBlockFromDisk(pindexDelete, consensusParams);
+    CBlockRef pblock(new CBlock());
+    ReadBlockFromDisk(*pblock, pindexDelete, consensusParams, false);
     if (!pblock)
         return AbortNode(state, "DisconnectTip(): Failed to read block");
     // Apply the block atomically to the chain state.
@@ -3278,10 +3245,10 @@ bool ConnectTip(CValidationState &state,
 
     // Read block from disk.
     int64_t nTime1 = GetStopwatchMicros();
-    CBlockRef pblockRef;
+    CBlockRef pblockRef(new CBlock());
     if (!pblock)
     {
-        pblockRef = ReadBlockFromDisk(pindexNew, chainparams.GetConsensus());
+        ReadBlockFromDisk(*pblockRef, pindexNew, chainparams.GetConsensus(), false);
         if (!pblockRef)
             return AbortNode(state, "ConnectTip(): Failed to read block");
         pblock = pblockRef.get();
