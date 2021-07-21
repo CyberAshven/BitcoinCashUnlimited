a8edf696d158ed935649448ecf2794169aa55706
George Bissias <bissias@gmail.com>
2021-07-21 17:37:49 -0400

    instrument latest error

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -438,6 +438,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
             if (pcursor->GetValue(diskindex))
             {
                 // Construct block index object
+                LOG(WB, "!!!This is the last place where I can see pindexNew being set to the wrong hash value: %s\n", diskindex.GetBlockHash().ToString());
                 CBlockIndex *pindexNew = InsertBlockIndex(diskindex.GetBlockHash());
                 pindexNew->pprev = InsertBlockIndex(diskindex.hashPrev);
                 pindexNew->nHeight = diskindex.nHeight;
