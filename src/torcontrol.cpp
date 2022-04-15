7975a1b2b3260d3e9e42ba8ffc3ae2d8fd9eb016
Griffith <probablyaplebeian@protonmail.com>
2022-04-14 21:26:03 -0700

    adjust TORv3 hidden service for BU

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -506,18 +506,20 @@ void TorController::auth_cb(TorControlConnection &_conn, const TorControlReply &
         }
 
         // Finally - now create the service
-        // No private key, generate one
-        if (private_key.empty()) {
+        if (private_key.empty()) // No private key, generate one
+        {
             // Explicitly request key type - see issue #9214
             private_key = "NEW:ED25519-V3";
         }
-        // Request onion service, redirect port.
-        // Note that the 'virtual' port is always the default port to avoid decloaking nodes using other ports.
-        _conn.Command(
-            strprintf("ADD_ONION %s Port=%i,127.0.0.1:%i", private_key, Params().GetDefaultPort(), GetListenPort()),
-            std::bind(&TorController::add_onion_cb, this, std::placeholders::_1, std::placeholders::_2));
-    } else {
-        LogPrintf("tor: Authentication failed\n");
+        // Request hidden service, redirect port.
+        // Note that the 'virtual' port doesn't have to be the same as our internal port, but this is just a convenient
+        // choice.  TODO; refactor the shutdown sequence some day.
+        _conn.Command(strprintf("ADD_ONION %s Port=%i,127.0.0.1:%i", private_key, GetListenPort(), GetListenPort()),
+            boost::bind(&TorController::add_onion_cb, this, boost::arg<1>(), boost::arg<2>()));
+    }
+    else
+    {
+        LOGA("tor: Authentication failed\n");
     }
 }
 
@@ -734,12 +736,10 @@ void TorController::Reconnect()
     }
 }
 
-fs::path TorController::GetPrivateKeyFile() {
-    return GetDataDir() / "onion_v3_private_key";
-}
-
-void TorController::reconnect_cb(evutil_socket_t fd, short what, void *arg) {
-    TorController *self = static_cast<TorController *>(arg);
+fs::path TorController::GetPrivateKeyFile() { return GetDataDir() / "onion_v3_private_key"; }
+void TorController::reconnect_cb(evutil_socket_t fd, short what, void *arg)
+{
+    TorController *self = (TorController *)arg;
     self->Reconnect();
 }
 
