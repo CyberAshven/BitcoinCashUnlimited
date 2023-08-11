ce6eac94561fda1f376097ad31b94c83cd011c68
Free Trade <freetrade.mc@gmail.com>
2023-08-11 13:00:56 +0700

    add randomx

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -11,8 +11,45 @@
 #include "hashwrapper.h"
 #include "tinyformat.h"
 #include "utilstrencodings.h"
+#include "randomx/randomx.h"
+#include <iostream>
 
-uint256 CBlockHeader::GetHash() const { return SerializeHash(*this); }
+//uint256 CBlockHeader::GetHash() const { return SerializeHash(*this); }
+
+uint256 CBlockHeader::GetHash() const
+{ 
+    //Just trying to run the randomx functions in the context of a BU build. 
+    
+    const char myKey[] = "RandomX example key";
+	const char myInput[] = "RandomX example input";
+	//char hash[RANDOMX_HASH_SIZE];
+    uint256 hash;
+
+    std::cout << "Get flags" << std::endl;
+	randomx_flags flags = randomx_get_flags();
+    flags=RANDOMX_FLAG_DEFAULT;
+    std::cout << flags << std::endl;
+    std::cout << "Allocate cache" << std::endl;
+    randomx_cache *myCache = randomx_alloc_cache(flags);
+	if (myCache == nullptr) {
+		std::cout << "Cache allocation failed" << std::endl;
+		return SerializeHash(*this);
+	}
+	
+	randomx_init_cache(myCache, &myKey, sizeof myKey);
+	randomx_vm *myMachine = randomx_create_vm(flags, myCache, NULL);
+	randomx_calculate_hash(myMachine, &myInput, sizeof myInput, &hash);
+	randomx_destroy_vm(myMachine);
+	randomx_release_cache(myCache);
+
+	//for (unsigned i = 0; i < RANDOMX_HASH_SIZE; ++i)
+	//	printf("%02x", hash[i] & 0xff);
+	//printf("\n");
+
+	//return hash;
+    return SerializeHash(*this);
+ }
+
 std::string CBlock::ToString() const
 {
     std::stringstream s;
